05e6d65aff1696f569e1c1f3f76630c22ef154e6
Biplab Roy <roy_biplab_89@yahoo.co.in>
2020-12-21 00:02:27 +0530

    img

--- 05. Codes/checksum.py
+++ 05. Codes/checksum.py
@@ -1,16 +1,25 @@
-packet = 'FFFFFFFFFF0686010700000843F00000'                                     # Incoming Packet
+packet = 'FFFFFFFFFF0686010700000843F0000076'                                   # Incoming Packet
 PL = "Packet length " + str(len(packet)) + " nibbles"                           # concatenate packet length
 print (PL)                                                                      # Print packet length
+for_check = packet [0:(len(packet)-2)]                                          # Separated data, excluding the checksum byte
+print (for_check)                                                               # print bytestream for checksum calculation
+incoming_checksum = packet [-2:]                                                # Last byte (FF) is incoming checksum
+print ("incoming checksum is " + (incoming_checksum))
 
 xor = 0                                                                         # value initiated with 0
 i = 0                                                                           # index initiated with 0
-while i < len(packet):                                                          # initiate xor operation till length of the data
-    xor = xor ^ ord(packet[i])                                                  # Bitwise xor
+while i < len(for_check):                                                       # initiate xor operation till length of the data
+    xor = xor ^ ord(for_check[i])                                               # Bitwise xor
     i += 1                                                                      # move pointer to next value
 
 checksum = hex (xor)                                                            # outputs 0xFF format
-final = checksum[2:4]                                                           # formatting required data
-print (final)                                                                   # print the formatted data
+calculated_checksum = checksum[2:]                                              # formatting checksum data
+print ("calculated checksum is " + (calculated_checksum))                       
+
+if (incoming_checksum == calculated_checksum):                                  # Compare checksums
+    print ("Checksum Match")                                                    # print result
+else:
+    print ("Checksum Mismatch")                                           
 
 
 
