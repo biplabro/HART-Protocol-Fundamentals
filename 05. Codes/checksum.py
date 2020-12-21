00b410b063e6c8b9ea69ae86b5ae897a6125e2c4
Biplab Roy <roy_biplab_89@yahoo.co.in>
2020-12-21 23:22:00 +0530

    Auto stash before merge of "main" and "origin/main"

--- 05. Codes/checksum.py
+++ 05. Codes/checksum.py
@@ -1,11 +1,28 @@
-packet = 'FFFFFFFFFF0686010700000843F0000076'                                   # Incoming Packet from HART transmitter
-print ("Packet length " + str(len(packet)) + " nibbles")                        # Print packet length
-for_check = packet [0:(len(packet)-2)]                                          # Separated data, excluding the checksum byte
-incoming_checksum = packet [-2:]                                                # Last byte (FF) is incoming checksum
-print ("incoming packet is: " + (packet))
-print ("incoming checksum is: " + (incoming_checksum))
-print ("checksum input is: " + (for_check))                                     # print bytestream for checksum calculation
+in_bytes = raw_input ("enter incoming bytestream: ")                           # user input syntax python 2.x
+#in_bytes = input ("enter incoming bytestream: ")                                # user input syntax python 3.x
 
+#in_bytes = 'FFFFFFFFFF0686010700000843F0000076'                                # Incoming Packet from HART transmitter
+
+########## segregation of information #################
+packet_len = len(in_bytes)/2                                     
+incoming_checksum = in_bytes [-2:]                                              # Last byte (HH) is incoming checksum, [reverse index:remaining]
+slave_id = in_bytes[12:14]
+command_code = in_bytes[14:16]
+data_bytes = in_bytes[22:-2]
+data_bytes_count = len(in_bytes[22:-2])/2                                       
+for_check = in_bytes [0:(len(in_bytes)-2)]                                      # Separated string, excluding the checksum byte
+                                               
+############## debugging information #################
+print ("Incoming packet is: " + (in_bytes))
+print ("Packet length is: " + str(packet_len) + " Bytes")                       # Print packet length
+print ("Slave address is: " + (slave_id))
+print ("Command code is: " + (command_code))
+print ("Data Bytes are: " + (data_bytes))
+print ("Total number of Data Bytes is: " + str(data_bytes_count) + " Bytes")
+print ("Incoming checksum is: " + (incoming_checksum))
+print ("Checksum input is: " + (for_check))                                     # print bytestream for checksum calculation
+
+############# error check module0 (checksum based) ###############
 xor = 0                                                                         # value initiated with 0
 i = 0                                                                           # index initiated with 0
 while i < len(for_check):                                                       # initiate xor operation till length of the data
@@ -15,14 +32,24 @@ while i < len(for_check):                                                       
 checksum = hex (xor)                                                            # outputs 0xFF format
 calculated_checksum = checksum[2:]                                              # formatting checksum data
 print ("calculated checksum is " + (calculated_checksum))                       
+#print ("calculated checksum is " + (checksum))                       
+
 
 if (incoming_checksum == calculated_checksum):                                  # Compare checksums
-    print ("Checksum Match")                                                    # print result
+    print ("Checksum Match")
+    cs = "00"                                                    # print result
 else:
     print ("Checksum Mismatch")                                           
+    cs = "01"
+
+############# error check module1 (value based) ###############
+
+############# error check module2 (voting based) ###############
+
+################# error check module3 (? based) ################
+
+################# formatted upstream data ####################
+upstream_data = str(slave_id + command_code + data_bytes + cs)
+print ("Upstream data is: " + upstream_data)
 
 
-
-# calculated checksum value is 0x77
-# incoming checksum is 0x76 (for debug purpuse)
-# edit the incoming packet to get "Checksum Match" confirmation
